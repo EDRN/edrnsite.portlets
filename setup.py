2234e441011ce874fd0d76f588fe22a84a0e33cc
Sean Kelly <sean.c.kelly@me.com>
2011-07-13 22:28:21 +0000

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os.path
 # ------------
 
 _name        = 'edrnsite.portlets'
-_version     = '1.1.2'
+_version     = '1.1.3'
 _description = 'Portlets for the EDRN Public Portal'
 _url         = 'http://cancer.jpl.nasa.gov/products/edrnsite-portlets'
 _downloadURL = 'http://oodt.jpl.nasa.gov/dist/edrnsite'
